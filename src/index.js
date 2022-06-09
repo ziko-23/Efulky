1d21a7b406f658cc8093c16ee36cacfbea4f8c64
ziko-23 <kabouchziko3@gmail.com>
2022-06-09 00:13:32 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Preloader from "./components/Preloader/Preloader";
+import Timer from "./components/Countdown/Timer";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>
+          Website
+          <br />
+          Coming Soon
+        </h1>
+        <Timer />
+        {/* <Optin /> */}
+        <Preloader />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
